a3eb67e61726a31187caf509ac8251b769f8f636
Sky00l <207410290@gms.tku.edu.tw>
2023-03-01 21:28:40 +0800

    w03-p3: supabase and pgadmin img

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,26 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    port: '5433',
-    database: 'wp2_demo_90',
-    password: '0000'
-})
+let pool;
+
+if(process.env.NODE_ENV === 'SUPABASE') {
+    pool = new Pool({
+        user: 'postgres',
+        host: process.env.SUPABASE_HOST,
+        port: '5432',
+        database: 'postgres',
+        password: '0000'
+    });
+    console.log(`Connecting PostgreSQL database `)
+}else{
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        port: '5433',
+        database: 'wp2_demo_90',
+        password: '0000'
+    });
+}
+
 
 console.log(`Connecting PostgreSQL database  ${pool.options.database}`);
 
